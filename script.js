3da0a783804e879467028ee0119f4e05f183fcc2
Lönnberg <bio20llg@ad.cs.umu.se>
2024-03-20 15:54:34 +0100

    hero slideshow done, need animation

--- script.js
+++ script.js
@@ -3,28 +3,34 @@ function HelloWorld(){
 }
 let slideIndex = 0;
 
-function prevSlide() {
-    showSlide(slideIndex -= 1);
-}
 
-function nextSlide() {
-    showSlide(slideIndex += 1);
-}
 
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+function currentSlide(n){
+	showSlide(slideIndex = n)
+}
 function showSlide(n) {
 	let slides = document.getElementsByClassName("hero");
-
+	let dots = document.getElementsByClassName("dot");
+	
 	if (n >= slides.length) { 
 		slideIndex = 0; 
 	}
 
 	if (n < 0) { 
-		slideIndex = slides.length - 1; 
+		slideIndex = slides.length -1; 
 	}
 	
+	//hides all slides
 	for (let i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
+		dots[i].classList.remove("active");
 	}
+
+	//Shows the slide of current index
 	slides[slideIndex].style.display = "grid";
+	dots[slideIndex].classList.add("active");
 }
 
