c3e0a1734640613ab02883f4254e2673c524c70f
Ludwig <Ludwig.lonnberg@gmail.com>
2024-04-01 18:41:59 +0200

    updates

--- script.js
+++ script.js
@@ -49,38 +49,74 @@ function inputFocus(x){
 function inputBlur(x){
 	x.style.backgroundColor ="transparent";
 }
+window.onload = () => {
+
+};
 
 //Logo hover
 function testHover(x){
-	x.style.transform = "scale(110%)"
-	x.style
+	x.style.transform = "scale(110%)";
 
 }
 function stopHover(x){
-	x.style.transform = "translate(0, 0)"
+	x.style.transform = "translate(0, 0)";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-	const products = document.querySelectorAll(".products");
+window.onload = () => { 
+	let navLinks = document.querySelectorAll(".nav a");
+	let products = document.querySelectorAll(".products");
+	let items = document.querySelectorAll(".item img");
+	let input = document.querySelectorAll("input")
 	
+	//changes apperance of navigation links on mouseover
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("mouseover", function() {
+            this.style.color = "red";
+        });
+
+        navLinks[i].addEventListener("mouseout", function() {
+            this.style.color = ""; 
+        });
+    }
+	//changes apperance of products on homepage on mouseover
+	for (let i = 0; i < products.length; i++) {
+        products[i].addEventListener("mouseover", function() {
+            this.style.transform ="translateY(-10px)";
+			
+        });
+
+        products[i].addEventListener("mouseout", function() {
+           this.style.transform = "translate(0, 0)";
+		   
+        });
+		
+    }
+
+	//changes apperance of all items on mouseover
+	for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener("mouseover", function() {
+            this.style.transform ="translateY(-10px)";
+			
+			
+        });
+
+        items[i].addEventListener("mouseout", function() {
+           this.style.transform = "translate(0, 0)";
+		   
+        });
+		
+    }
+	//changes apperance of input fields on focus
+	for (let i = 0; i < input.length; i++) {
+        input[i].addEventListener("focus", function() {
+            this.style.backgroundColor = "white";
+        });
+
+        input[i].addEventListener("blur", function() {
+            this.style.backgroundColor = ""; 
+        });
+    }
+};
       
-	products.forEach(product => {
-		product.addEventListener("mousemove", function(event) {
-			moveObject(event, product);
-		});
-	
-		product.addEventListener("mouseleave", function() {
-			resetPosition(product);
-		});
-	});
-});
-      
-function moveObject(event, product) {
-	product.style.transform ="translateY(-10px)";
-}
-      
-function resetPosition(product) {
-	product.style.transform = "translate(0, 0)";
-}
 
       
